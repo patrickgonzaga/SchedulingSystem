4052b18da32f9dbb7f8e5308028b27d8c49eb05d
P.Gonzaga <59947366+patrickgonzaga@users.noreply.github.com>
2023-04-21 11:54:45 +0800

    Add project files.

---
+++ Data.EntityModel/Models/DemoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data.Web.Models
+{
+    public class DemoContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite(@"Data Source=./Database/Database.db");
+    }
+}
